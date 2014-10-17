be1247dc6e4ae1d217a8fd38c4d7410904bf1514
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2014-10-17 15:42:39 +0300

    new script for data_of_player. Necessary it  improve and test!

---
+++ Scripts/data_of_player.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Merry on 17.10.2014.
+ */
+var one_player;
+var  two_player;
+
+function name_of_player(){
+
+do {
+    one_player = document.getElementById("player_one");
+    two_player = document.getElementById("player_two");
+}while(one_player==""||two_player=="");
+
+    alert("test_function");
+
+
+}
+
+
+
