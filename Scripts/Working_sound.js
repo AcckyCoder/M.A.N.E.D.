34c3c88c28767d20f6b1959fb2af8016cd87eb89
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2014-10-27 17:35:47 +0200

    Added Script - Working sound

---
+++ Scripts/Working_sound.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Merry on 27.10.2014.
+ */
+document.getElementsByClassName("button").onclick = function()
+{
+    var my_sound = document.getElementById("button_click");
+
+    if(my_sound.paused == true)
+
+    {
+
+        document.getElementById("button_click").play();
+    }
+
+    else if (my_sound.paused == false)
+
+    {
+
+        document.getElementById("button_click").pause();
+
+}
+};
