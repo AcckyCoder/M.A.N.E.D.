9af551224ec013ae2c6e341d869afff820d12133
outdream <udjen_13@mail.ru>
2014-10-27 15:53:25 +0200

    create  drawBoard function

--- Scripts/createCanvasMap.js
+++ Scripts/createCanvasMap.js
@@ -63,5 +63,20 @@ function createHexGrid(){
             }
         });
     }
+    function drawBoard(canvasContext, width, height) {
+        var i,
+            j;
+
+        for(i = 0; i < width; ++i) {
+            for(j = 0; j < height; ++j) {
+                drawHexagon(
+                    ctx,
+                    i * hexRectangleWidth + ((j % 2) * hexRadius),
+                    j * (sideLength + hexHeight),
+                    false
+                );
+            }
+        }
+    }
 
 }
