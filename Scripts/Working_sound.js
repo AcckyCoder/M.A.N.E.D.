416b2a0b6bd2fef0d2c2924f89151f095ce43ba7
Natalya <natashalysakova@gmail.com>
2014-10-27 18:54:24 +0200

    audio not tur-off fix

--- Scripts/Working_sound.js
+++ Scripts/Working_sound.js
@@ -1,22 +1,16 @@
 /**
  * Created by Merry on 27.10.2014.
  */
-document.getElementsByClassName("button").onclick = function()
-{
+document.getElementsByClassName("button").onclick = function () {
     var my_sound = document.getElementById("button_click");
 
-    if(my_sound.paused == true)
-
-    {
-
+    if (my_sound.paused == true) {
         document.getElementById("button_click").play();
     }
 
-    else if (my_sound.paused == false)
-
-    {
+    else if (my_sound.paused == false) {
 
         document.getElementById("button_click").pause();
 
-}
+    }
 };
