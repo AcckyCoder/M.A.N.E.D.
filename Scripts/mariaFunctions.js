4f6e2e3f5950ab427e09420727b2eab723d5e828
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-01-08 00:06:54 +0200

    added function carryOutAgitation()

--- Scripts/mariaFunctions.js
+++ Scripts/mariaFunctions.js
@@ -1,6 +1,3 @@
-/**
- * Created by Natalya on 07.01.2015.
- */
 
 
 function first_city_id(){
@@ -16,7 +13,7 @@ function first_city_id(){
     var first = Math.floor(Math.random() * found_city.length);
     console.log(map[found_city[first]].cityName);
     return found_city[first];
-}
+}//присвоить начальный город игроку
 
 function getCityIdByName(name)
 {
@@ -28,10 +25,38 @@ function getCityNameById(id)
     return map[id].cityName;
 }
 
-function startstep(){
+function startstep()//установить значение иттерации
+{
   var firststep=1;
   document.getElementById('step').innerText=firststep;
 }
+
+
 function showResourse(){
     document.getElementById('coin_out').innerText= player.money;
+    document.getElementById('nameplayer').innerHTML= player.name;
+} //показать текущие ресурсы юзера
+
+function carryOutAgitation(money,index_city,type_number){ //на агитацию  нужны деньги и указать где проходит. Агитация 2х видов
+    //либо 1-организовать концерт,либо 2-повысить всем зарплаты
+ var rest;//остаток
+ var current_balance;//текущий баланс юзера
+ current_balance=player.money;
+
+    if (type_number != 1) {
+        rest = current_balance - money;
+        map[index_city].happy=+50;
+        map[index_city].popularity=+10;
+
+
+    } else {
+        rest = current_balance - money;
+        map[index_city].happy=+50;
+        map[index_city].salary=+money;
+        map[index_city].popularity=+10;
+        map[index_city].helth=+10;  //есть деньги, есть возможность купить лекарство
+
+    }
+    player.money=rest;
+
 }
