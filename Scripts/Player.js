6a20c93d8b53ced63e8608fcda8992e1247ce0a4
Natalya <natashalysakova@gmail.com>
2015-01-07 17:51:48 +0200

    add player object

---
+++ Scripts/Player.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Natalya on 07.01.2015.
+ */
+
+var player =
+    {
+        "name": "vanya",
+        "color": "red",
+        "money": 50000,
+        "tree": 50000,
+        "coal": 50000,
+        "wheat": 50000,
+        "gas": 50000,
+        "rock": 50000
+    }
+    ;
