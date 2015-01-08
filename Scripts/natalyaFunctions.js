02cc30d5b75d8269f6a6e26ea49323138532c7f9
Natalya <natashalysakova@gmail.com>
2015-01-08 10:11:15 +0200

    fix "полтора зелекопа :))"

--- Scripts/natalyaFunctions.js
+++ Scripts/natalyaFunctions.js
@@ -64,7 +64,7 @@ function levelUp(cityID) {
         {
             map[cityID].level++;
             player.money -= levelUpPrice[level-1];
-            map[cityID].popularity *= map[cityID].level;
+            map[cityID].popularity *= Math.round(map[cityID].level);
             map[cityID].taxes += 1;
             map[cityID].salary += Math.round((map[cityID].salary*map[cityID].level)*0.2);
         }
@@ -124,7 +124,7 @@ function GetCityProfit(cityId)
 
     var workingPeople =  city.popularity * (city.unemployment/100.0);
     var theirSalary = city.salary * (city.taxes/100.0);
-    var cityProfit = workingPeople * theirSalary;
+    var cityProfit = Math.round(workingPeople) * theirSalary;
     return Math.round(cityProfit);
 }
 
@@ -151,18 +151,18 @@ function isPlayerHasMoreCities() {
 function updateCityParameters(city) {
 
     //вычисляем вероятность возникновения события/
-    var possible = Math.random() * 100;
+    var possible = Math.round(Math.random() * 100);
 
     if (possible % 5 == 0) {
         //если здоровье горожан в порядке, то рождаются новые горожане
         if (city.health >= 50) {
-            var born = Math.random() * city.health;
+            var born = Math.round(Math.random() * city.health);
             city.popularity+=born;
             AddHappy(city, 1);
         }
         else {
             //иначе они дохнут
-            var died = Math.random() * (50 - city.health);
+            var died = Math.round(Math.random() * (50 - city.health));
             city.popularity-= died;
             AddHappy(city, -1);
 
@@ -181,10 +181,10 @@ function updateCityParameters(city) {
 
     //если в городе высоая преступность
     if (city.crime > 50) {
-        possible = Math.random() * 100;
+        possible = Math.round(Math.random() * 100);
         if (possible % 5 == 0) {
             //кто-то может погибнуть
-            var died = Math.random() * (50 - city.crime);
+            var died = Math.round(Math.random() * (50 - city.crime));
             city.popularity-= died;
             if (city.popularity <= 0) {
                 if (!isPlayerHasMoreCities())
@@ -206,17 +206,17 @@ function updateCityParameters(city) {
 
     if (city.taxes > 15)
     {
-        possible = Math.random() * 100;
+        Math.round(Math.random() * 100);
         if(possible%5 == 0)
         {
             AddHappy(city, -1);
-            city.popularity -= Math.random()*15;
+            city.popularity -= Math.round(Math.random()*15);
         }
     }
     else if(city.taxes <= 15)
     {
         AddHappy(city, 1);
-        city.popularity += Math.random()*15;
+        city.popularity += Math.round(Math.random()*15);
     }
 
     if(city.happy > 50)
@@ -242,28 +242,30 @@ function updateCityParameters(city) {
 }
 
 function AddHappy(city, count) {
-    if (city.happy + count < 100 && city.happy + count > 0) {
-        city.happy += count;
-    }
-    else if (city.happy + count > 100)
+
+    city.happy += count;
+
+
+    if (city.happy > 100)
     {
         city.happy = 100;
     }
-    else
+
+    if(city.happy < 0)
     {
         city.happy = 0;
     }
 }
 
 function AddUnemployment(city, count) {
-    if (city.unemployment + count < 100 && city.unemployment + count > 0) {
+
         city.unemployment += count;
-    }
-    else if (city.unemployment + count > 100)
+
+    if (city.unemployment > 100)
     {
         city.unemployment = 100;
     }
-    else
+    if (city.unemployment < 0)
     {
         city.unemployment = 0;
     }
@@ -271,14 +273,14 @@ function AddUnemployment(city, count) {
 
 
 function AddHealth (city, count) {
-    if (city.health + count < 100 && city.health + count > 0) {
+
         city.health += count;
-    }
-    else if (city.health + count > 100)
+
+    if (city.health > 100)
     {
         city.health = 100;
     }
-    else
+    if (city.health < 0)
     {
         city.health = 0;
     }
@@ -286,17 +288,17 @@ function AddHealth (city, count) {
 
 
 function AddCrime (city, count) {
-    if (city.crime + count < 100 && city.crime + count > 0) {
-        city.crime += count;
+
+    city.crime += count;
+
+
+    if (city.crime > 100) {
+        city.crime = 100;
     }
-    else {
-        if (city.crime + count > 100) {
-            city.crime = 100;
-        }
-        else {
-            city.crime = 0;
-        }
-    }
+    if (city.crime < 0)
+        city.crime = 0;
+
+
 }
 
 function updateResourceParameters(resource) {
