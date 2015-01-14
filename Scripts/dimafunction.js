fec41733ac2a2090fb89e0cb09515eb6a0081f07
Дмитрий <sheplyakov2011@gmail.com>
2015-01-14 23:05:57 +0300

    Реализовал функция аврия на шахте

--- Scripts/dimafunction.js
+++ Scripts/dimafunction.js
@@ -1,6 +1,4 @@
-/**
- * Created by Dmitriy on 09.01.2015.
- */
+
 
 function crashInSawmill(){// авария на лесопилке
     var id= randomUserCities();
@@ -14,6 +12,7 @@ function crashInSawmill(){// авария на лесопилке
         player.money -= Math.round(Math.random()*money);
         player.tree-=Math.round(Math.random()*trees);
         addHappy(city,-happy);
+        KillSomePeople();
         showEventPopup("Произошла авария на лесопилке", eventType.negative);
     }
     showResInPanel();
@@ -41,4 +40,44 @@ function Play(id){
         SoundList[id].play();		// воспроизовдим звук по его ID в списке
         console.log('plaed: ' +id); // для тестирования
     }
+}
+
+function getAllUserCities()
+{
+    var cities = [];
+    var k = 0;
+    for (var i = 0; i < map.length; i++)
+    {
+        if(map[i].owner == player.name)
+        {
+            cities[k] = map[i];
+        }
+    }
+    return cities;
+}
+function KillSomePeople()
+{
+    var cities = getAllUserCities();
+    for(var i=0; i< cities.length; i++)
+    {
+        cities[i].popularity -= Randomfactors(0,10);
+    }
+}
+
+function crashInMine(){// авария на шахте
+    var id= randomUserCities();
+    var pos=Math.round(Math.random()*100);
+    var money = player.money;
+    var coal = player.coal;
+    var city = map[id];
+    var happy = Randomfactors(4,9);
+    if(pos%20==0)
+    {
+        player.money -= Math.round(Math.random()*money);
+        player.coal-=Math.round(Math.random()*coal);
+        addHappy(city,-happy);
+        KillSomePeople()
+        showEventPopup("Произошла авария на шахте", eventType.negative);
+    }
+    showResInPanel();
 }
